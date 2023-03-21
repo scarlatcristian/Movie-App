22665335828fc40fe1da6605cc9d94639b321637
cristi <scarlat.cristian.gabriel@gmail.com>
2023-03-21 18:34:58 +0200

    added to the DOM the top movies

--- app.js
+++ app.js
@@ -8,13 +8,52 @@ const SEARCH_API =
 
 const form = document.getElementById("form");
 const search = document.getElementById("search");
+const main = document.getElementById("main");
+
+const getClassByRate = (vote) => {
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 5) {
+    return "orange";
+  } else return "red";
+};
+
+const showMovies = (movies) => {
+  main.innerHTML = "";
+
+  movies.forEach((movie) => {
+    const { title, poster_path, overview } = movie;
+
+    const vote_average = Number(movie.vote_average.toFixed(1));
+
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+
+    movieEl.innerHTML = `
+        <img
+          src="${IMG_PATH + poster_path}"
+          alt="${title}"
+        />
+        <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+          <h3>Overview</h3>
+            ${overview}
+        </div>
+    `;
+
+    main.appendChild(movieEl);
+  });
+};
 
 // Fetch the movies
 const getMovies = async (url) => {
   const res = await fetch(url);
   const data = await res.json();
 
-  console.log(data.results);
+  showMovies(data.results);
 };
 
 // Search for the movie title submitted
